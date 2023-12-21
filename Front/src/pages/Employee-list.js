3ed9c647b9dc0b2d6c64af70c03678549b517dc7
Eric BOUILLAUT <e.bouillaut@yahoo.fr>
2023-12-21 09:17:24 +0100

    Add sort table

--- Front/src/pages/Employee-list.js
+++ Front/src/pages/Employee-list.js
@@ -2,12 +2,30 @@ import React, { useState, useEffect } from "react";
 
 const pageSizeOptions = [10, 25, 50, 100];
 
+const compareValues = (a, b, field, sortOrder) => {
+  if (!a[field] || !b[field]) return 0;
+
+  if (field === "startDate" || field === "dateOfBirth") {
+    const dateA = new Date(a[field]);
+    const dateB = new Date(b[field]);
+    return sortOrder === "asc" ? dateA - dateB : dateB - dateA;
+  }
+
+  const valueA = a[field].toString().toLowerCase();
+  const valueB = b[field].toString().toLowerCase();
+  if (valueA < valueB) return sortOrder === "asc" ? -1 : 1;
+  if (valueA > valueB) return sortOrder === "asc" ? 1 : -1;
+  return 0;
+};
+
 const EmployeeList = ({ useBackend }) => {
   const [employees, setEmployees] = useState([]);
   const [filteredEmployees, setFilteredEmployees] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [pageSize, setPageSize] = useState(10);
   const [currentPage, setCurrentPage] = useState(0);
+  const [sortField, setSortField] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
 
   const loadEmployees = async () => {
     try {
@@ -20,26 +38,80 @@ const EmployeeList = ({ useBackend }) => {
   };
 
   useEffect(() => {
+    setCurrentPage(0); // Réinitialiser la page courante à 0 lorsque pageSize change
+  }, [pageSize, searchTerm]);
+
+  useEffect(() => {
     useBackend
       ? loadEmployees()
       : setEmployees(JSON.parse(localStorage.getItem("employees")));
   }, [useBackend]);
 
   useEffect(() => {
-    const filtered = employees.filter((employee) =>
-      Object.values(employee)
-        .join(" ")
-        .toLowerCase()
-        .includes(searchTerm.toLowerCase())
-    );
+    const filtered = employees
+      .filter((employee) =>
+        Object.values(employee)
+          .join(" ")
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase())
+      )
+      .sort((a, b) => compareValues(a, b, sortField, sortOrder));
     setFilteredEmployees(filtered);
-  }, [searchTerm, employees]);
+  }, [searchTerm, employees, sortField, sortOrder]);
 
   const pageCount = Math.ceil(filteredEmployees.length / pageSize);
   const employeesToShow = filteredEmployees.slice(
     currentPage * pageSize,
     (currentPage + 1) * pageSize
   );
+
+  const handleSort = (field) => {
+    const order = field === sortField && sortOrder === "asc" ? "desc" : "asc";
+    setSortField(field);
+    setSortOrder(order);
+  };
+
+  const formatHeader = (header) => {
+    switch (header) {
+      case "firstName":
+        return "First Name";
+      case "lastName":
+        return "Last Name";
+      case "startDate":
+        return "Start Date";
+      case "dateOfBirth":
+        return "Date of Birth";
+      case "zipCode":
+        return "Zip Code";
+      default:
+        return header.charAt(0).toUpperCase() + header.slice(1);
+    }
+  };
+
+  const renderHeader = () => {
+    const headers = [
+      "firstName",
+      "lastName",
+      "startDate",
+      "department",
+      "dateOfBirth",
+      "street",
+      "city",
+      "state",
+      "zipCode",
+    ];
+    return headers.map((header) => (
+      <th key={header} onClick={() => handleSort(header)}>
+        {formatHeader(header)}
+        {sortField === header && (
+          <i
+            className={`fas fa-sort-${sortOrder === "asc" ? "up" : "down"}`}
+          ></i>
+        )}
+        {sortField !== header && <i className="fas fa-sort"></i>}
+      </th>
+    ));
+  };
 
   const renderPaginationButtons = () => {
     let buttons = [];
@@ -101,45 +173,37 @@ const EmployeeList = ({ useBackend }) => {
   };
 
   return (
-    <div>
-      <h1>Current Employees</h1>
-      <div>
-        <label htmlFor="search">Search:</label>
-        <input
-          type="text"
-          id="search"
-          placeholder="Enter name to search"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
+    <div className="employee-list-container">
+      <h1 className="title">Current Employees</h1>
+      <div className="search-and-size">
+        <div className="search-box">
+          <label htmlFor="search">Search:</label>
+          <input
+            type="text"
+            id="search"
+            placeholder="..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+        <div className="page-size-selector">
+          Show
+          <select
+            value={pageSize}
+            onChange={(e) => setPageSize(Number(e.target.value))}
+          >
+            {pageSizeOptions.map((size) => (
+              <option key={size} value={size}>
+                {size}
+              </option>
+            ))}
+          </select>
+          entries
+        </div>
       </div>
-      <div>
-        Show
-        <select
-          value={pageSize}
-          onChange={(e) => setPageSize(Number(e.target.value))}
-        >
-          {pageSizeOptions.map((size) => (
-            <option key={size} value={size}>
-              {size}
-            </option>
-          ))}
-        </select>
-        entries
-      </div>
-      <table>
+      <table className="employee-table">
         <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Start Date</th>
-            <th>Department</th>
-            <th>Date of Birth</th>
-            <th>Street</th>
-            <th>City</th>
-            <th>State</th>
-            <th>Zip Code</th>
-          </tr>
+          <tr>{renderHeader()}</tr>
         </thead>
         <tbody>
           {employeesToShow.map((employee) => (
@@ -157,7 +221,7 @@ const EmployeeList = ({ useBackend }) => {
           ))}
         </tbody>
       </table>
-      <div>
+      <div className="pagination-container">
         <p>
           Showing {Math.max(1, currentPage * pageSize + 1)} to{" "}
           {Math.min(filteredEmployees.length, (currentPage + 1) * pageSize)} of{" "}
