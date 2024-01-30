72cad179575d4c043dfa90a66536443563f4065f
Eric BOUILLAUT <e.bouillaut@yahoo.fr>
2024-01-30 10:01:18 +0100

    Add header/footer+css in Error.js

--- Front/src/pages/Error.js
+++ Front/src/pages/Error.js
@@ -1,12 +1,20 @@
 import { NavLink } from "react-router-dom";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 //Composant de la page d'erreur
 function Error() {
   return (
-    <div className="error">
-      <h2>Erreur 404</h2>
-      <p>La page que vous demandez n'existe pas.</p>
-      <NavLink to="/">Retourner sur la page d'accueil</NavLink>
+    <div className="home-container">
+      <Header />
+      <main className="home-body">
+        <div className="error">
+          <h2>Erreur 404</h2>
+          <p>La page que vous demandez n'existe pas.</p>
+          <NavLink to="/">Retourner sur la page d'accueil</NavLink>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
